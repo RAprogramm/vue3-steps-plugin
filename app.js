d253ff04d7e4729376775b425752cae00cf53834
RAprogramm <andrey.rozanov.vl@gmail.com>
2021-02-17 08:54:13 +0900

    add mobile responsive

--- app.js
+++ app.js
@@ -14,41 +14,33 @@ const app = Vue.createApp({
     }
   },
   methods: {
+    setActive(i) {
+      this.activeIndex = i
+    },
     prev() {
-      // когда нажимаем кнопку назад
       if (this.activeIndex !== 0) {
         this.activeIndex--
       }
     },
     reset() {
-      // начать заного
       this.isActiveStep = true
       this.activeIndex = 0
     },
     nextOfFinish() {
-      // кнопка вперед или закончить
       if (this.activeIndex !== this.steps.length - 1) {
         this.activeIndex++
       } else {
         this.isActiveStep = false
       }
-    },
-    setActive(i) {
-      // когда нажимаем на определенный шаг
-      this.activeIndex = i
     }
   },
   computed: {
-    // тут стоит определить несколько свойств:
-    // 1. текущий выбранный шаг
     activeStep() {
       return this.steps[this.activeIndex]
     },
-    // 2. выключена ли кнопка назад
     isButtonDisabled() {
       return this.activeIndex === 0
     },
-    // 3. находимся ли мы на последнем шаге
     isLastStep() {
       return this.activeIndex === this.steps.length - 1
     }
@@ -74,13 +66,13 @@ const app = Vue.createApp({
             :disabled="isButtonDisabled"
             class="btn"
             @click="prev"
-          >Назад</button>
+          >Back</button>
           <button class="btn primary" @click="nextOfFinish">
-            {{ isLastStep ? 'Закончить' : 'Вперед' }}
+            {{ isLastStep ? 'Finish' : 'Next' }}
           </button>
         </div>
         <div v-else>
-          <button class="btn" @click="reset">Начать заново</button>
+          <button class="btn" @click="reset">Start again</button>
         </div>
     </div>
   `
